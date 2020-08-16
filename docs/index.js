7a5bfcf16f4dc5bc405196e137b8f8a7044a72e6
Saul Vega <saulv@MacBook-Pro.local>
2020-08-16 11:04:08 -0600

    added the close mobile menu with outside click

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,25 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".navBar .navBar_linksWrapper");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navBar .navBar_linksWrapper");
+const navLink = document.querySelector("#navLink-dropdown");
+const dropdown = document.querySelector(".dropdown_content");
 
+const navSlide = () => {
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
   });
 };
 
 const activeDropdownLink = () => {
-  const navLink = document.querySelector("#navLink-dropdown");
-  const dropdown = document.querySelector(".dropdown_content");
-
   navLink.addEventListener("click", () => {
     dropdown.classList.toggle("activeDropdown");
   });
 };
 
+window.addEventListener("click", function (event) {
+  // const activeNav = document.querySelector(".navBar_linksWrapper .nav-active");
+  if (event.target != nav && event.target != burger) {
+    nav.classList.remove("nav-active");
+  }
+});
 navSlide();
 activeDropdownLink();
